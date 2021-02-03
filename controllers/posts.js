b6649973808ac25aac721789e6be880c63ce8dcf
Panda64 <JaylenSchelb@yahoo.com>
2021-02-02 22:53:29 -0800

    Progress on displaying post

--- controllers/posts.js
+++ controllers/posts.js
@@ -14,4 +14,14 @@ module.exports = (app) => {
     })
   });
 
+  app.get('/', (req, res) => {
+    Post.find({}).lean()
+      .then(posts => {
+        res.render('posts-index', { posts });
+      })
+      .catch(err => {
+        console.log(err.message);
+      })
+  })
+
 };
